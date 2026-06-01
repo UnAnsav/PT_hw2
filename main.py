4e464120d7aba2ac122be6f54657a5d23563c6b1
Андрей Савинов <74003441+UnAnsav@users.noreply.github.com>
2026-06-01 19:51:16 +0300

    Добавлен класс Ingredient

---
+++ main.py
@@ -0,0 +1,30 @@
+class Ingredient:
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        val = float(value)
+        if val <= 0:
+            raise ValueError("Количество должно быть положительным")
+        self._quantity = val
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+
+    def __eq__(self, other):
+        if isinstance(other, Ingredient):
+            return self.name == other.name and self.unit == other.unit
+        return False
+    
+#fl = Ingredient('мука', 2, 'кг')
+#print(fl)
